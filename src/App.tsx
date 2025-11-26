fa7d9cb7221fe8a86c82ffb6191918061a50252e
Pedro Antônio <pedroantonioytbr@gmail.com>
2025-11-26 18:58:48 -0300

    feat: configure routing system with React Router
    - Installed React Router
    - Added routes: Dashboard, About, Login, Register and NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+/* Folhas de Estilos */
 import './App.css'
 
+/* React Router Dom */
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+/* Páginas */
+import Dashboard from './pages/Dashboard/Dashboard'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import About from './pages/About/About'
+import NotFound from './pages/NotFound/NotFound'
+
 function App() {
+
   return (
     <main className='App'>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   )
+
 }
 
 export default App
